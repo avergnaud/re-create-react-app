98dd86fa202470b8ad39d4674ebc07de684c50e3
Adrien Vergnaud <adrien@MacBook-Air-de-Adrien.local>
2019-10-26 11:33:54 +0200

    HTML ReactJS sans jsx

--- App.js
+++ App.js
@@ -5,7 +5,21 @@ const rootContainer = document.querySelector('#root');
 const elements = React.createElement(
     'div',
     { className: 'App' },
-    'Hello World'
+    React.createElement(
+        'header',
+        { className: 'App-header' },
+        [
+            React.createElement(
+                'img',
+                { src: 'logo.svg', className: 'App-logo', alt: 'logo' }
+            ),
+            React.createElement(
+                'p',
+                {},
+                'Hello  World'
+            )
+        ]
+    )
 );
 
 ReactDOM.render(elements, rootContainer);
